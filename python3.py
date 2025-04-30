89c7261887c1e9bcecbd1aefdfa46b5f7c0375fa
emiliadelrealg <edelrealgom@alumni.unav.es>
2025-04-30 10:19:05 +0200

    Update python3.py
    Port parameters

--- python3.py
+++ python3.py
@@ -5,8 +5,10 @@ Created on Wed Apr 30 10:00:06 2025
 @author: edelrealgom
 """
 
+
 import serial # library used to communicate with serial port
 import re # library used to extract data from string
 import tkinter as tk
 import sys
 
+f = ""
